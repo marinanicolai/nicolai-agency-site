0a73b6d99dfd3ea76031412467989cf2fe87f28c
Marina Nicolai <nicolaimarina@gmail.com>
2025-08-11 22:16:42 -0400

    adding the images and getting deploying it

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
-  site: "https://www.nicolaiagency.com", 
+  site: "https://www.nicolaiagency.com",
   integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)), // ✅ absolute path
+      },
+    },
   },
 });
